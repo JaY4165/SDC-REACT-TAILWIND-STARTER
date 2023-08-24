a540773709e46e6f1c8187b1cebec13fed67a19d
JaY4165 <harsha.gontu@gmail.com>
2023-08-24 22:54:09 +0530

    React + Tailwind starter files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="flex w-screen h-screen justify-center items-center text-5xl">
+        Hello Guys
+      </h1>
+    </>
+  );
+}
+
+export default App;
